64d11e130cab3b99f591ff35cef3274d0813f00e
martimcabral <mfcmartimyt@gmail.com>
2025-07-29 20:02:05 +0000

    🌹🏁🌹

---
+++ timers/hazbin_hotel.js
@@ -0,0 +1,23 @@
+var Element = document.getElementById("hhtimer")
+var countDownDate = new Date("Oct 29, 2025 00:00:00").getTime();
+
+function updateTimer() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    Element.innerHTML = days + "D " + hours + "H "
+        + minutes + "M " + seconds + "S ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        Element.innerHTML = "EXPIRED";
+    }
+}
+
+updateTimer();
+var x = setInterval(updateTimer, 1000);
--- timers/nextf1race.js
+++ timers/nextf1race.js
@@ -1,4 +1,5 @@
-var countDownDate = new Date("Sep 24, 2025 00:00:00").getTime();
+var Element = document.getElementById("f1timer");
+var countDownDate = new Date("Aug 3, 2025 13:00:00").getTime();
 
 function updateTimer() {
     var now = new Date().getTime();
@@ -9,12 +10,12 @@ function updateTimer() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("timer").innerHTML = days + "D " + hours + "H "
+    Element.innerHTML = days + "D " + hours + "H "
         + minutes + "M " + seconds + "S ";
 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("timer").innerHTML = "EXPIRED";
+        Element.innerHTML = "EXPIRED";
     }
 }
 
